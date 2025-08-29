153d8004766f67e40fb90a3dbe93ccb99ba578d3
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-08-29 20:16:27 +0300

    chore(msw): enable  MSW mocking in app entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,24 @@
-import { StrictMode } from 'react';
+// eslint-disable-next-line import/order
+import { scan } from 'react-scan';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './app';
+import { App } from './app';
+import { enableMocking } from './tests/mocks';
 
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+scan({
+  enabled: true,
+});
+
+const root = document.getElementById('root');
+if (!root) throw new Error('No root element found');
+
+enableMocking().then(() => {
+  createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+});
