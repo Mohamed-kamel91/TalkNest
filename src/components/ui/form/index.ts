166ce093528db7ec4aca17bcc8af47bc3cf925da
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-09-11 20:11:21 +0300

    feat(ui): create from components and move label to folder

---
+++ src/components/ui/form/index.ts
@@ -0,0 +1,5 @@
+export { Form } from './form';
+export { Field } from './field';
+export { Input } from './input';
+export { Label } from './label';
+export { FieldErrorText } from './field-error-text';
