94c07198b2930b1a8368e244de249cf8fa9ab95b
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-09-21 22:56:56 +0300

    refactor(mocks): create auth handler folder

---
+++ src/tests/mocks/handlers/auth/constants.ts
@@ -0,0 +1,8 @@
+const BASE_PATH = '/api/auth';
+
+export const authPaths = {
+  login: `${BASE_PATH}/login`,
+  register: `${BASE_PATH}/register`,
+  logout: `${BASE_PATH}/logout`,
+  me: `${BASE_PATH}/me`,
+};
