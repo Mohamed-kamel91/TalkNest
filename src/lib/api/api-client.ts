4b9bdc9ccd8f586bfc4f8a6a4769acbe43496b4b
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-09-21 23:42:53 +0300

    refactor(api): refactor axios instance configuration / interceptors

--- src/lib/api/api-client.ts
+++ src/lib/api/api-client.ts
@@ -1,7 +1,33 @@
-import Axios, { type AxiosInstance } from 'axios';
+import Axios, {
+  type AxiosInstance,
+  type CreateAxiosDefaults,
+} from 'axios';
 
 import { env } from '@/config/env';
+import { paths } from '@/config/paths';
 
-export const api: AxiosInstance = Axios.create({
+const axiosConfig: CreateAxiosDefaults = {
   baseURL: env.API_URL,
-});
+  withCredentials: true,
+  headers: {
+    'Content-type': 'application/json',
+    Accept: 'application/json',
+  },
+  timeout: 5000,
+};
+
+export const api: AxiosInstance = Axios.create(axiosConfig);
+
+api.interceptors.response.use(
+  (response) => response.data, // we only care about the data, we leave the state of api to reat query
+  (error) => {
+    if (error.response?.status === 401) {
+      const searchParams = new URLSearchParams();
+      const redirectTo =
+        searchParams.get('redirectTo') || window.location.pathname;
+      window.location.href = paths.auth.login.getHref(redirectTo);
+    }
+
+    return Promise.reject(error);
+  },
+);
