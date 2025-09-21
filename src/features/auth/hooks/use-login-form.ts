f409462614d6ce1323a1440b5ef74ff5dcc00207
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-09-21 23:51:33 +0300

    feat(auth): create custom hook for login form

---
+++ src/features/auth/hooks/use-login-form.ts
@@ -0,0 +1,41 @@
+import { useNavigate, useSearchParams } from 'react-router';
+
+import { paths } from '@/config/paths';
+import { useLogin } from '@/lib/api/auth';
+import { useZodForm } from '@/lib/hooks/use-zod-form';
+
+import { defaultLoginValues } from '../constants';
+import { loginFormSchema, type LoginInput } from '../types';
+
+export const UseLoginForm = () => {
+  const login = useLogin();
+
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const redirectTo =
+    searchParams.get('redirectTo') || paths.app.root.getHref();
+
+  const form = useZodForm(loginFormSchema, {
+    defaultValues: defaultLoginValues,
+    initialFocus: 'email',
+  });
+
+  const onSubmit = (data: LoginInput) => {
+    login.mutate(data, {
+      onSuccess: () => {
+        navigate(redirectTo, {
+          state: { from: redirectTo },
+          replace: true,
+        });
+      },
+    });
+  };
+
+  return {
+    login,
+    redirectTo,
+    ...form,
+    onSubmit,
+  };
+};
