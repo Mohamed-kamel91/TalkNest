745a4b0806a2747f4b2afa4b6af9b6c989a7c9ea
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-09-22 23:10:02 +0300

    test: add data generators for tests

---
+++ src/tests/data-generators.ts
@@ -0,0 +1,20 @@
+import { faker } from '@faker-js/faker';
+
+const generateUser = () => ({
+  id: faker.string.uuid(),
+  firstName: faker.person.firstName(),
+  lastName: faker.person.lastName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+  role: 'ADMIN',
+  bio: faker.lorem.paragraph(),
+  createdAt: Date.now(),
+});
+
+export const createUser = <
+  T extends Partial<ReturnType<typeof generateUser>>,
+>(
+  overrides?: T,
+) => {
+  return { ...generateUser(), ...overrides };
+};
