c7e4fa104c366b39d8264b95f7cf62d135362a72
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-09-25 00:58:39 +0300

    refactor(test): enhance type safety to user data generator

--- src/tests/data-generators.ts
+++ src/tests/data-generators.ts
@@ -1,12 +1,14 @@
 import { faker } from '@faker-js/faker';
 
+import type { User } from '@/types/api';
+
 const generateUser = () => ({
   id: faker.string.uuid(),
   firstName: faker.person.firstName(),
   lastName: faker.person.lastName(),
   email: faker.internet.email(),
   password: faker.internet.password(),
-  role: 'USER',
+  role: 'USER' as User['role'],
   bio: faker.lorem.paragraph(),
   createdAt: Date.now(),
 });
@@ -15,6 +17,6 @@ export const createUser = <
   T extends Partial<ReturnType<typeof generateUser>>,
 >(
   overrides?: T,
-) => {
+): User => {
   return { ...generateUser(), ...overrides };
 };
