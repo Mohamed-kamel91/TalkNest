5ca10f942723b8d08973718973f7415357b8e960
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-09-30 22:00:25 +0300

    test(ui): add aria-label to PageLoader for testing

--- src/components/ui/loaders/page-loader.tsx
+++ src/components/ui/loaders/page-loader.tsx
@@ -2,7 +2,10 @@ import { Spinner } from './spinner/spinner';
 
 export const PageLoader = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div
+      className="flex h-screen w-screen items-center justify-center"
+      aria-label="Page loading"
+    >
       <Spinner size="xl" />
     </div>
   );
