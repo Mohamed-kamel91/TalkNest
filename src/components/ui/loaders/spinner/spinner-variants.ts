b0b4d3017da385b8d6dc9d6be31472d30d502de3
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-09-30 22:01:54 +0300

    refactor(ui): add slots to  spinner variants

--- src/components/ui/loaders/spinner/spinner-variants.ts
+++ src/components/ui/loaders/spinner/spinner-variants.ts
@@ -1,18 +1,35 @@
 import { tv, type VariantProps } from 'tailwind-variants';
 
 const spinnerVariants = tv({
-  base: 'inline-block animate-spin rounded-full border-solid border-e-transparent motion-reduce:animate-[spin_1.5s_linear_infinite]',
+  slots: {
+    container: 'inline-flex items-center justify-center',
+    icon: 'animate-spin rounded-full border-solid border-e-transparent motion-reduce:animate-[spin_1.5s_linear_infinite]',
+  },
   variants: {
     size: {
-      sm: 'size-4',
-      md: 'size-5',
-      lg: 'size-6',
-      xl: 'size-7',
+      sm: {
+        icon: 'size-4',
+      },
+      md: {
+        icon: 'size-5',
+      },
+      lg: {
+        icon: 'size-6',
+      },
+      xl: {
+        icon: 'size-7',
+      },
     },
     color: {
-      default: 'color-current',
-      primary: 'color-primary',
-      secondary: 'color-secondary',
+      default: {
+        icon: 'text-current',
+      },
+      primary: {
+        icon: 'text-primary',
+      },
+      secondary: {
+        icon: 'text-secondary',
+      },
     },
   },
   defaultVariants: {
