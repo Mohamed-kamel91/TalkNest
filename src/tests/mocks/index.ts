48832a35df4991b5f79a1adbad88677f0e74c5da
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-12-22 02:49:14 +0200

    feat(mock): seed db with data when initializing msw on app start

--- src/tests/mocks/index.ts
+++ src/tests/mocks/index.ts
@@ -1,10 +1,13 @@
 import { env } from '@/config/env';
 
+import { seedDb } from './db/seeds';
+
 export const enableMocking = async () => {
   if (env.ENABLE_API_MOCKING) {
     const { worker } = await import('./browser');
     const { initializeDb } = await import('./db');
     await initializeDb();
+    await seedDb();
     return worker.start();
   }
 };
