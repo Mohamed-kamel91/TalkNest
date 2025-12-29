659bb1db76e1040305f3a18eb3157f59db8fae25
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-12-29 10:47:32 +0200

    feat(ui): add iconOnly prop

--- src/components/ui/sidebar/sidebar-trigger.tsx
+++ src/components/ui/sidebar/sidebar-trigger.tsx
@@ -14,11 +14,11 @@ export const SidebarTrigger = ({
 
   return (
     <Button
+      className={cn('size-7', className)}
+      iconOnly
+      variant="ghost"
       data-sidebar="trigger"
       data-slot="sidebar-trigger"
-      variant="ghost"
-      size="icon"
-      className={cn('size-7', className)}
       onClick={(event) => {
         onClick?.(event);
         toggleSidebar();
