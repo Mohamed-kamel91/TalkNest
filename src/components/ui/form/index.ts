cb654d2845038789a02724be9605687db8bcd8a2
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2026-01-19 14:25:50 +0200

    chore(ui): add select to form index file

--- src/components/ui/form/index.ts
+++ src/components/ui/form/index.ts
@@ -2,4 +2,6 @@ export { Form } from './form';
 export { Field } from './field';
 export { Input } from './input';
 export { Label } from './label';
+export { Textarea } from './textarea';
+export * from './select';
 export { FieldErrorText } from './field-error-text';
