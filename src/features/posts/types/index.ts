6812e5deb8f52d5831c9aa9242c4e565fcd0edb4
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2026-01-20 14:16:53 +0200

    feat: add create post form zod schema

--- src/features/posts/types/index.ts
+++ src/features/posts/types/index.ts
@@ -1,3 +1,31 @@
-import type { sortLabels } from '../constants';
+import z from 'zod';
 
-export type SortBy = (typeof sortLabels)[number];
+import { TOPIC_VALUES, type SORT_VALUES } from '../constants';
+
+export type SortBy = (typeof SORT_VALUES)[number];
+
+export const creatPostFormSchema = z.object({
+  title: z
+    .string()
+    .trim()
+    .min(1, { message: 'Title is required.' })
+    .max(100, { message: 'Title must be less than 100 characters.' }),
+
+  content: z
+    .string()
+    .trim()
+    .min(1, { message: 'Content is required.' })
+    .max(5000, {
+      message: 'Post content is too long (max 5000 characters).',
+    }),
+
+  topic: z
+    .enum(TOPIC_VALUES, {
+      message: 'Please select a valid topic from the list.',
+    })
+    .refine((val) => val !== '', {
+      message: 'Please select a topic.',
+    }),
+});
+
+export type CreatePostInput = z.infer<typeof creatPostFormSchema>;
